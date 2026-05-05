947201637a66a878288a728ed6df6a2b8eecbead
rodrigomartins8741 <rodrigo.martins8741@gmail.com>
2026-05-04 21:36:37 -0300

    valor max menor que min

--- script.js
+++ script.js
@@ -21,22 +21,3 @@ function resetNumber(){
    location.reload()
 }
 
-/*function buttonGenerate(){
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max = Math.floor(document.querySelector('.input-max').value)
-    const numberDrawn = document.querySelector('.number-drawn')
-
-    // ✅ VALIDAR PRIMEIRO
-    if(max > min){
-        const result = Math.floor(Math.random() * (max - min + 1)) + min
-        numberDrawn.innerHTML = result
-    }
-    else{
-        alert('Valor máximo tem que ser maior que o valor mínimo')
-        // ✅ NÃO ALTERA O VALOR
-    }
-}
-
-function resetNumber(){
-   location.reload()
-}*/
